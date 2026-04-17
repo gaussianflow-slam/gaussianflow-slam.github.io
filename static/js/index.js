c2f6d4f928d780dd10472c9614f9531740d7409a
SeoDU <dongukseo@kaist.ac.kr>
2026-04-17 19:06:57 +0900

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,29 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+$(document).ready(function() {
+    // Navbar burger toggle
+    $(".navbar-burger").click(function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+    });
+
+    var options = {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      loop: true,
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 3000,
+    }
+
+    // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    for(var i = 0; i < carousels.length; i++) {
+      carousels[i].on('before:show', state => {
+        console.log(state);
+      });
+    }
+
+    bulmaSlider.attach();
+})
